f9d6d415c454dbd08cce5fa13084b60bc754e2bb
pedro augusto montes sales <nerdindiano@gmail.com>
2021-08-10 13:13:08 -0300

    inserindo ID com uuid

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use(categoriesRoutes);
+app.use("/categories", categoriesRoutes);
 
 app.listen(7474, () => console.log("server esta rodando"));
